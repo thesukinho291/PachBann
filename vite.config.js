022c36ca54d382db93137e2516289647eb08a91e
thesukinho291 <dpachelligois@gmail.com>
2026-05-15 18:50:55 -0300

    Build login page in Vite multi-page config

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,17 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
 export default defineConfig({
   root: '.',
   publicDir: 'public',
   build: {
     outDir: 'dist',
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        login: resolve(__dirname, 'login.html'),
+      },
+    },
   },
   server: {
     port: 3000,
